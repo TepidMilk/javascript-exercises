9e3b1ba1bb3379b035beb904f77965aa6c1c74a4
Noah Kingrey <ivorykingrey@gmail.com>
2023-03-29 15:16:08 -0500

    edit repeatString.js
    this change allows the code to pass the first 4 tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let repeater = "";
+    for (i = 1; i <= num; i++){
+        repeater += string;
+    }
+    return repeater;
 };
 
 // Do not edit below this line
