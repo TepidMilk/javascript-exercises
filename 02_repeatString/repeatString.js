dc4bbfddc1c2e78f5ba32b0202cf01456546621c
Noah Kingrey <ivorykingrey@gmail.com>
2023-03-29 15:34:03 -0500

    edit repeatString.js to pass all tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,10 +1,12 @@
 const repeatString = function(string, num) {
     let repeater = "";
+    if (num < 0){
+        return "ERROR"
+    }
     for (i = 1; i <= num; i++){
         repeater += string;
     }
     return repeater;
-};
-
+}
 // Do not edit below this line
 module.exports = repeatString;
