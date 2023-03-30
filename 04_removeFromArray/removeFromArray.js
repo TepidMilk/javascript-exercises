ad101e3132379712fad5ed575e5ff40ee34683de
Noah Kingrey <ivorykingrey@gmail.com>
2023-03-30 18:31:55 -0500

    edit removeFromArray.js to pass test

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array) {
+    let removed = []
+    const args = Array.from(arguments)
+    for (i = 0; i < array.length; i++){
+        if (!args.includes(array[i])){
+            removed.push(array[i]);
+        }
+    }
+    return removed;
 };
 
 // Do not edit below this line
