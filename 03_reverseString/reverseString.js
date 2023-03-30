a6af6f6b13de88b11b0747becdb303d2fe7ca62c
Noah Kingrey <ivorykingrey@gmail.com>
2023-03-29 22:00:42 -0500

    edit reverseString.js to pass tests

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,11 @@
-const reverseString = function() {
-
-};
+const reverseString = function(string) {
+    let text = string
+    let reversed = ""
+    for (i = 1; i < text.length + 1; i++){
+        reversed += text.substr(-i, 1);
+    }
+    return reversed;
+}
 
 // Do not edit below this line
 module.exports = reverseString;
