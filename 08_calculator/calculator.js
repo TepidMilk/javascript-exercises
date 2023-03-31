137aac3dfde634e4c5d49d618f8758b701b00029
Noah Kingrey <ivorykingrey@gmail.com>
2023-03-30 22:00:06 -0500

    edit calculator.js to pass tests

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,39 @@
 const add = function() {
-	
+	let args = Array.from(arguments);
+  let add = 0
+  args.forEach((item) => add += item);
+  return add;
 };
 
 const subtract = function() {
-	
+	return arguments[0] - arguments[1]
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	let sum = 0
+  array.forEach((item) => sum += item)
+  return parseInt(sum)
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let multiple = 1
+  array.forEach((item) => multiple *= item)
+  return parseInt(multiple)
 };
 
 const power = function() {
-	
+	return arguments[0]**arguments[1]
 };
 
 const factorial = function() {
-	
+	let factor = 1
+  if (arguments[0] == 0){
+    return 1
+  }
+  for (i = 1; i <= arguments[0]; i++){
+    factor *= i;
+  }
+  return factor
 };
 
 // Do not edit below this line
