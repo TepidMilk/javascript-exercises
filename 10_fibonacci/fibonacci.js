f900e03a447526f32754f72321b0ca69c7e5483e
Noah Kingrey <ivorykingrey@gmail.com>
2023-04-04 15:28:26 -0500

    edit fibonacci.js to pass tests

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    let fibSequence = [1, 1]
+    if (n < 0){
+        return "OOPS"
+    }
+    for (i = 1; i < n - 1; i++){
+        fibSequence.push(fibSequence[i] + fibSequence[i-1])
+    }
+    
+    return fibSequence.pop()
 };
+
+console.log(fibonacci(-5))
 
 // Do not edit below this line
 module.exports = fibonacci;
