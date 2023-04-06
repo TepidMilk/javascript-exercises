4f88f764ad2666fdead16e9930911fe640a8f7d6
Noah Kingrey <ivorykingrey@gmail.com>
2023-04-05 20:09:05 -0500

    edit findTheOldest.js to pass test

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,38 @@
-const findTheOldest = function() {
+const people = [
+    {
+      name: "Carly",
+      yearOfBirth: 1066,
+    },
+    {
+      name: "Ray",
+      yearOfBirth: 1962,
+      yearOfDeath: 2011,
+    },
+    {
+      name: "Jane",
+      yearOfBirth: 1912,
+      yearOfDeath: 1941,
+    },
+  ]
 
+const findTheOldest = function(people) {
+    let persons = []
+    let date = new Date
+    for (i = 0; i < people.length; i++){
+        let person = people[i]
+        person.yearOfDeath ?
+        persons.push(person) : 
+        person.yearOfDeath = date.getFullYear()
+        console.log(person.yearOfDeath)
+        persons.push(person)
+    }
+    let oldestPerson = persons.reduce((currentOldest, currentIndex) => 
+    ((currentOldest.yearOfDeath - currentOldest.yearOfBirth) < (currentIndex.yearOfDeath - currentIndex.yearOfBirth)) ?
+    currentIndex : currentOldest)
+    console.log(oldestPerson)
+    return oldestPerson
 };
 
+console.log(findTheOldest(people))
 // Do not edit below this line
 module.exports = findTheOldest;
